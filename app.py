33ef9722634b00ed7ac423a47e50ba9f488d9223
Nikhil <37044074+ND15@users.noreply.github.com>
2021-07-17 14:17:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -60,20 +60,55 @@ def binomial():
     return render_template('binomial.html', prediction_text='None')
 
 
+def uniform_calcs(a, b, x=0, y=0, *distr):
+    f_x = 1 / (b - a)
+    res = 0
+    if "less" or "Less" in distr:
+        res = (x - 0) * (f_x)
+        return res
+
+    elif "percentile" or "Percentile" in distr:
+        perc = x / 100
+        res = (perc * (1 / f_x)) + a
+        return res
+
+    elif "more" or "More" in distr:
+        res = (b - x) * f_x
+        return res
+
+    elif "conditional" or "Conditional" in distr:
+        f_x = 1 / (b - y)
+        res = (b - x) * f_x
+        return res
+
+    elif "less than greater than" or "Less Than Greater Than" in distr:
+        res = (y - x) * f_x
+
+    elif "mean" or "Mean" in distr:
+        res = (a + b) / 2
+        return res
+
+    elif "stdev" or "Stdev" in distr:
+        res = math.sqrt((b - a) ** 2 / 12)
+        return res
+
+
 @app.route('/uniform', methods=['GET', 'POST'])
 def uniform():
-    if 'n' in request.form:
-        n = request.form['n']
+    if 'a' in request.form:
+        a = request.form['a']
+        b = request.form['b']
         x = request.form['x']
-        p = request.form['p']
-        n = int(n)
-        x = int(x)
-        p = float(p)
+        y = request.form['y']
+        a = float(a)
+        b = float(b)
+        x = float(x)
+        y = float(y)
         type_str = request.form['type']
         if type_str in ['mean', 'variance']:
-            res = binom_num(n, p, (1 - p), type_str)
+            res = uniform_calcs(a, b, x, y, type_str)
         else:
-            res = binom(n, x, p, type_str)
+            res = uniform_calcs(a, b, x, y, type_str)
         return render_template('uniform.html', prediction_text='{}: {}'.format(type_str, res))
     return render_template('uniform.html', prediction_text='None')
 
